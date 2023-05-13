094d15e12328c21f9ccfd6d7ba03b9b7dfbbe9d4
Michael Ghebranious <ghebraniousmike@gmail.com>
2023-05-13 15:38:56 -0500

    Initial Commit

---
+++ Soccerdle/SoccerdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerdleApp.swift
+//  Soccerdle
+//
+//  Created by Michael Ghebranious on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SoccerdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
